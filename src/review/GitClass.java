d11c47edb117b816ce42d6f9ff9117f2b86e02af
azizi121 <117691856+azizi121@users.noreply.github.com>
2022-11-10 23:38:43 -0800

    Update GitClass.java
    delete it eventually

--- src/review/GitClass.java
+++ src/review/GitClass.java
@@ -2,13 +2,5 @@ package review;
 
 public class GitClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public void test() {
-		System.out.println("this method is for review branch");
-	}
 
 }
